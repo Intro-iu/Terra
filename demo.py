4f93030e514c035e391e785da262fed131b310e4
Abyss Skb <abyssskb@outlook.com>
2024-03-09 08:33:56 +0800

    修改了部分规范，如使用字典，以及获取列表长度的方式。

--- demo.py
+++ demo.py
@@ -3,9 +3,11 @@ from world import *
 
 world = World()
 world.spawn(berry(1, 1))
+world.spawn(Adam)
+world.spawn(Eva)
 
-world.reproduce(world.human[0], world.human[1])
-print(world.plant)
-print(world.human)
+world.reproduce(world.species['Human'][0], world.species['Human'][1])
+print(world.species['berry'])
+print(world.species['Human'])
 
 world.run()
--- world.py
+++ world.py
@@ -3,23 +3,23 @@ from life import *
 class World:
     def __init__(self):
         self.time = 0
-        self.human = [Adam, Eva]
-        self.plant = []
+        self.species = {}
         
-
     def spawn(self, object):
-        if object.__class__.__name__ == 'Human':
-            self.human.append(object)
-        if object.__class__.__name__ == 'berry':
-            self.plant.append(object)
+        # 判断世界中原先是否存在同类物种，如果不存在则新建
+        objectName = object.__class__.__name__
+        if objectName not in self.species:
+            self.species[objectName] = []
+        
+        self.species[objectName].append(object) # 添加物种
 
     # 人类繁衍
     def reproduce(self, Father, Mother):
-        self.spawn(Human(self.human.__len__(), time = self.time, Father=Father, Mother=Mother))
+        self.spawn(Human(len(self.species['Human']), time = self.time, Father=Father, Mother=Mother))
 
     def timePass(self):
         self.time += 1
-        for human in self.human:
+        for human in self.species['Human']:
             human.grow()
             human.target()  # AI决策人类行动
             human.hug -= 0.1
@@ -28,9 +28,9 @@ class World:
                 human.age += 1
 
             if human.hug <= 0:
-                self.human.remove(human)
+                self.species['Human'].remove(human)
                 print('Human', human.id, 'died.', 'Score:', human.score())
-                if self.human.__len__() == 0:
+                if len(self.species['Human']) == 0:
                     print('The last human died.')
                     break
             else:
